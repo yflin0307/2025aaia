99c4f796d21ea1bf06a544da75e46154e6060299
yflin0307 <13751001@me.mcu.edu.tw>
2025-05-09 11:49:37 +0800

    week12

---
+++ week12-6.cpp
@@ -0,0 +1,13 @@
+//week12_06
+class Solution{
+   public:
+      vector<int> buildArray(vector<int>& nums){
+         int N = nums.size();
+         vector<int> ans(N);
+         for(int i=0; i<N; i++){
+            ans[i] = nums[nums[i]];
+         }
+         return ans;
+      }
+
+};
